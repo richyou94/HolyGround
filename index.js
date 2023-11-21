9d27894f670f19d1c9e118461b3e27a2d7a8d665
Yi Chan Richard You <richardbassyou@Yis-MacBook-Pro.local>
2023-11-21 14:06:34 -0800

    mongoose connected

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/holyGround')
+.then(() => {
+    console.log('mongoose connected');
+})
+.catch((err) => {
+    console.log('got error!');
+    console.log(err);
+})
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+    app.set('view engine', 'ejs');
 
-app.get('/blog', (req, res) => {
-    res.send('blog site')
-}) 
+    app.get('/blog', (req, res) => {
+        res.send('blog site')
+    })
 
-app.listen(3000, () => {
-    console.log("APP is listening on PORT 3000!")
-})  
+    app.listen(3000, () => {
+        console.log("APP is listening on PORT 3000!")
+    })

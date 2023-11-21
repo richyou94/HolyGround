58331060c90176d470efe582f1a9907f8e9c6790
Yi Chan Richard You <richardbassyou@Yis-MacBook-Pro.local>
2023-11-21 13:49:32 -0800

    initial git init process for creating website

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/blog', (req, res) => {
+    res.send('blog site')
+})
+
+app.listen(3000, () => {
+    console.log("APP is listening on PORT 3000!")
+})  
